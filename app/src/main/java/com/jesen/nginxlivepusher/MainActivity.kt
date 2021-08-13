4ba19ed02856c1ffb16cb7221571409a4bdf8c79
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-13 12:05:34 +0800

    [NginxPusher] 01. create project

---
+++ app/src/main/java/com/jesen/nginxlivepusher/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.jesen.nginxlivepusher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.jesen.nginxlivepusher.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'nginxlivepusher' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'nginxlivepusher' library on application startup.
+        init {
+            System.loadLibrary("nginxlivepusher")
+        }
+    }
+}
